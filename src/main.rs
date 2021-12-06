ef85a8b051d87c5f9627ef613f615fe381ed53a6
Marcelo Nicolas Gomez Rivera <mng@Marcelos-MacBook-Pro.local>
2021-12-06 09:11:59 +0000

    Solve part 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 // Advent of Code 2021, Day 6
 // https://adventofcode.com/2021/day/6
 
+use std::collections::BTreeMap;
 use std::{io::BufRead, str::FromStr};
-
-const COUNTER_RESET_VALUE: usize = 6;
-const NEW_FISH_COUNTER_VALUE: usize = 8;
 
 #[derive(Debug, Clone, Copy)]
 struct LanternFish {
@@ -16,15 +14,48 @@ impl LanternFish {
         Self { counter }
     }
 
-    pub fn advance_counter(&mut self) -> Option<LanternFish> {
-        if self.counter > 0 {
-            self.counter -= 1;
-            None
-        } else {
-            // We've hit the end of our counter, time to reproduce!
-            self.counter = COUNTER_RESET_VALUE;
-            Some(LanternFish::new(NEW_FISH_COUNTER_VALUE))
+    /// Calculates how many fish we would end up with after <days> days
+    /// starting from this fish
+    pub fn num_fish_after(&self, days: usize) -> usize {
+        self.num_fish_after_impl(days, &mut BTreeMap::new())
+    }
+
+    /// Calculates how many fish we would end up with after <days> days
+    /// starting from this fish
+    fn num_fish_after_impl(&self, days: usize, memo: &mut BTreeMap<(usize, usize), usize>) -> usize {
+        if days <= self.counter {
+            return 1;
         }
+
+        if let Some(result) = memo.get(&(self.counter, days)) {
+            return *result;
+        }
+
+        // Account for the number of fish we will create just by resetting
+        // due to the amount of days that we're asked to advance
+        let num_children = (days - self.counter - 1) / 7 + 1;
+
+        // Now for each of these children we calculate how many fish they would have created
+        // For this we need to know which day (relative to now) each child was created
+        // and advance its counter by <days> - <created_day>
+        // Add 1 to account for self
+        1 + (0..num_children)
+            // Calculate when it would have been created
+            .map(|i| 7 * i + self.counter + 1)
+            // Recursion!
+            .map(|creation_day| {
+                let new_counter = 8;
+                let new_days = days.saturating_sub(creation_day);
+                let result = LanternFish::new(new_counter)
+                    .num_fish_after_impl(new_days, memo);
+                // Only memoize things that will need recursion
+                if new_days > new_counter {
+                    memo.entry((new_counter, new_days)).or_insert(result);
+                }
+                result
+            })
+            // Add it all up
+            .fold(0, |sum, num_children| sum + num_children)
     }
 }
 
@@ -34,30 +65,58 @@ fn parse_input_line(s: &str) -> anyhow::Result<Vec<LanternFish>> {
         .collect::<Result<_, _>>()?)
 }
 
-fn solve_part_1(mut fish: Vec<LanternFish>) -> Vec<LanternFish> {
-    (0..80).for_each(|_| {
-        let new_fish = fish
-            .iter_mut()
-            .filter_map(|f| f.advance_counter())
-            .collect::<Vec<LanternFish>>();
-
-        fish.extend(new_fish);
-    });
-
-    fish
-}
-
 fn main_impl() -> anyhow::Result<()> {
     let mut line = String::new();
     std::io::stdin().lock().read_line(&mut line)?;
 
     let fish = parse_input_line(&line)?;
 
-    println!("Part 1 solution {}", solve_part_1(fish).len());
+    println!(
+        "Part 1 solution {}",
+        fish.iter()
+            .map(|f| f.num_fish_after(80))
+            .fold(0, |sum, fish_count| sum + fish_count),
+    );
+
+    println!(
+        "Part 2 solution {}",
+        fish.iter()
+            .map(|f| f.num_fish_after(256))
+            .fold(0, |sum, fish_count| sum + fish_count),
+    );
 
     Ok(())
 }
 
 fn main() {
     main_impl().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_num_fish_after() {
+        assert_eq!(
+            LanternFish::new(3).num_fish_after(1),
+            1
+        );
+    }
+
+    #[test]
+    fn test_num_fish_after_counter_hits_zero() {
+        assert_eq!(
+            LanternFish::new(3).num_fish_after(3),
+            1
+        );
+    }
+
+    #[test]
+    fn test_num_fish_after_counter_passes_zero() {
+        assert_eq!(
+            LanternFish::new(3).num_fish_after(4),
+            2
+        );
+    }
 }
