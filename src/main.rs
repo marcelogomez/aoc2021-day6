6fabef96c42fd6ebbbcf445d4cd987fb4f2acbe2
Marcelo Nicolas Gomez Rivera <mng@Marcelos-MacBook-Pro.local>
2021-12-06 07:13:06 +0000

    Parsse input file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 // Advent of Code 2021, Day 6
 // https://adventofcode.com/2021/day/6
 
+use std::{io::BufRead, str::FromStr};
+
 #[derive(Debug, Clone, Copy)]
 struct LanternFish {
     counter: usize,
 }
 
 impl LanternFish {
-    #[allow(dead_code)]
     pub fn new(counter: usize) -> Self {
-        Self {
-            counter,
-        }
+        Self { counter }
     }
 }
 
+fn parse_input_line(s: &str) -> anyhow::Result<Vec<LanternFish>> {
+    Ok(s.split(',')
+        .map(|n| usize::from_str(n).map(LanternFish::new))
+        .collect::<Result<_, _>>()?)
+}
+
+fn main_impl() -> anyhow::Result<()> {
+    let mut line = String::new();
+    std::io::stdin().lock().read_line(&mut line)?;
+
+    let fish = parse_input_line(&line);
+    println!("Parsed input {:#?}", fish);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    main_impl().unwrap();
 }
