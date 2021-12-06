c22d4a0dabcf64139b0afcd956655edbb31ddcec
Marcelo Nicolas Gomez Rivera <mng@Marcelos-MacBook-Pro.local>
2021-12-06 07:04:41 +0000

    Bootstrap model for lanternfish

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 // Advent of Code 2021, Day 6
 // https://adventofcode.com/2021/day/6
 
+#[derive(Debug, Clone, Copy)]
+struct LanternFish {
+    counter: usize,
+}
+
+impl LanternFish {
+    #[allow(dead_code)]
+    pub fn new(counter: usize) -> Self {
+        Self {
+            counter,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
